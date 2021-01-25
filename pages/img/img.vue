110b3a3ba3d458aa245c89a4e6674056fc4ecfa8
王锦涛 <jintao9@staff.weibo.com>
2021-01-26 00:38:42 +0800

    update

--- pages/img/img.vue
+++ pages/img/img.vue
@@ -1,6 +1,15 @@
 <template>
 	<view>
 		
+		<button type="primary" class="content" hover-class="">按一按</button>
+		<hr/>
+		<view>
+			<map style="width: 100%;height:600px;" :latitude="latitude" :longitude="longitude" :marker="covers" enable-3D="true" show-compass="true"
+			 
+			 enable-location="true">
+				腾讯地图
+			</map>
+		</view>
 	</view>
 </template>
 
@@ -8,7 +17,18 @@
 	export default {
 		data() {
 			return {
-				
+				id:0,
+				latitude:38.9,
+				longitude:116.9,
+				covers:[{
+					latitude:55.09,
+					longitude:116.92,
+					iconPath:"/static/pic/0.jpg"
+				},{
+					latitude:62.92,
+					longitude:115.099,
+					iconPath:"/static/pic/1.jpg"
+				}]
 			}
 		},
 		methods: {
